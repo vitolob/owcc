19c9fab238804185f8f2b5dad2415530a4afc2ff
Victorino Machava <vitomachava@gmail.com>
2018-06-27 00:07:42 +0200

    Add code to populate select field plus some code refactoring

--- src/js/main/index.js
+++ src/js/main/index.js
@@ -1,24 +1,64 @@
+const apiURLs = require('./api.js');
+
+// HTML elements
 const form = document.querySelector('#currencyForm');
 const amountField = document.querySelector('#inputAmount');
 const resultField = document.querySelector('#outputCurrency')
-const apiURLs = require('./api.js');
+const selectFields = document.querySelectorAll('#currencyForm select');
 
-form.addEventListener('submit', event => {
-  event.preventDefault();
+class App {
 
-  const fetchUrl = apiURLs.baseURL + apiURLs.exchangeRate + 'USD_MZN,MZM_USD' + apiURLs.extra;
-  const AMOUNT = Number(amountField.value);
-
-  function handleData(data) {
-    const XR = Number(data.USD_MZN) // Exchange Rate
-
-    resultField.value = XR * AMOUNT;
+  init() {
+    this.fetchConversionFactor();
+    this.fetchCurrency();
   }
 
-  fetch(fetchUrl)
-    .then(response => {
-      return response.json();
-  }).then(handleData);
+  fetchConversionFactor() {
 
-  // amount.value = ''; // Clear the input field
-});
+    form.addEventListener('submit', event => {
+      event.preventDefault();
+
+      const fetchUrl = apiURLs.baseURL + apiURLs.exchangeRate + 'USD_MZN,MZM_USD' + apiURLs.extra;
+      const AMOUNT = Number(amountField.value);
+
+      const handleData = function (data) {
+        const XR = Number(data.USD_MZN) // Exchange Rate
+
+        resultField.value = XR * AMOUNT;
+      }
+
+      fetch(fetchUrl)
+        .then(response => {
+          return response.json();
+      }).then(handleData);
+
+      // amount.value = ''; // Clear the input field
+      });
+  }
+
+  fetchCurrency() {
+    const currencyUrl = apiURLs.baseURL + apiURLs.currencyList;
+
+    const handleData = function(data) {
+      const results = data.results;
+      const currencyList = Object.keys(results);
+      let currencyOptionHTML = '';
+
+      for (let currency of currencyList) {
+        currencyOptionHTML += `<option value=${currency}>${currency} - ${results[currency].currencyName}</option>`;
+      }
+
+       selectFields.forEach(elem => {
+         elem.innerHTML = currencyOptionHTML;
+       });
+    }
+
+    fetch(currencyUrl)
+      .then(response => {
+      return response.json();
+    }).then(handleData);
+  }
+}
+
+AppInstance = new App();
+AppInstance.init();
